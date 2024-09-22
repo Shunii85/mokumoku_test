7a92c3fdbd87af55301ce9eeee828524963456b5
shunii <shunii85@outlook.jp>
2024-09-22 16:57:37 +0900

    update

--- helloworld.js
+++ helloworld.js
@@ -2,19 +2,3 @@ console.log("Hello World");
 
 // ランダムな2桁の数字を生成する
 const num = Math.floor(Math.random() * 90) + 10;
-
-export const add = (a, b) => a + b;
-
-add(1, 2); // 3
-
-export const sub = (a, b) => a - b;
-
-sub(1, 2); // -1
-
-export const mul = (a, b) => a * b;
-
-mul(1, 2); // 2
-
-export const div = (a, b) => a / b;
-
-div(1, 2); // 0.5
